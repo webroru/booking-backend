5b33c0d382378caef11cd3d27bb8a6bef1d36c80
Igor Nemykin <i.nemykin@devcaz.com>
2022-10-24 17:01:14 +0200

    friendsofsymfony/rest-bundle

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api', name: 'api_')]
+class BookingController extends AbstractController
+{
+    #[Route('/booking', name: 'app_booking', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/BookingController.php',
+        ]);
+    }
+}
