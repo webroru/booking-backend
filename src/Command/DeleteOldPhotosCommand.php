03cb11005340d7deb613001d39071f47a08e6dd2
Igor Nemykin <i.nemykin@devcaz.com>
2022-11-22 18:20:24 +0100

    add delete-old-photos command

---
+++ src/Command/DeleteOldPhotosCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+#[AsCommand(
+    name: 'app:delete-old-photos',
+    description: 'Remove photos of IDs and passports',
+)]
+class DeleteOldPhotosCommand extends Command
+{
+    public function __construct(
+        private readonly ParameterBagInterface $params,
+        private readonly Filesystem $filesystem,
+        string $name = null,
+    ) {
+        parent::__construct($name);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $currantDay = (new \DateTime('+2 days'))->format('Y-m-d');
+        $photosDirectory = $this->params->get('photos_directory');
+
+        if (!$photosDirectory) {
+            $io->error("Param 'photos_directory' is not configured");
+            return Command::FAILURE;
+        }
+
+        $io->success("Photos for $currantDay will be removed");
+        $this->filesystem->remove("$photosDirectory/$currantDay");
+        $io->success('All photos have been removed.');
+
+        return Command::SUCCESS;
+    }
+}
