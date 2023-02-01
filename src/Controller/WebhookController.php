4bd76d2cb0123278f4e9e432ff469886b3479871
Igor Nemykin <i.nemykin@devcaz.com>
2023-02-01 14:30:52 +0100

    add PaymentService

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -4,13 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Providers\Booking\Beds24\Entity\InvoiceItem;
-use App\Providers\Booking\BookingInterface;
-use App\Service\ClientService;
-use Psr\Log\LoggerInterface;
-use Stripe\Webhook;
+use App\Service\PaymentService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,9 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class WebhookController extends AbstractController
 {
     public function __construct(
-        private readonly BookingInterface $booking,
-        private readonly LoggerInterface $logger,
-        private readonly ClientService $clientService,
+        private readonly PaymentService $paymentService,
     ) {
     }
 
@@ -31,22 +24,8 @@ class WebhookController extends AbstractController
         $endpointSecret = $this->getParameter('endpoint_secret');
         $payload = $request->getContent();
         $signature = $request->server->get('HTTP_STRIPE_SIGNATURE');
-        $event = Webhook::constructEvent($payload, $signature, $endpointSecret);
-        $paymentIntent = $event->data->object;
-        $bookingId = (int) $paymentIntent->metadata?->bookingId;
-        $clientName = $paymentIntent->metadata?->client;
-        $amount = $paymentIntent->amount;
-        if (!$bookingId) {
-            $this->logger->error('PaymentIntent does not contain bookingId', ['paymentIntent' => print_r($paymentIntent, true)]);
-            throw new BadRequestException('PaymentIntent does not contain bookingId');
-        }
-        if (!$clientName) {
-            $this->logger->error('PaymentIntent does not contain client name', ['paymentIntent' => print_r($paymentIntent, true)]);
-            throw new BadRequestException('PaymentIntent does not client name');
-        }
-        $this->booking->setToken($this->clientService->getTokenByName($clientName)->getToken());
-        $this->booking->addInvoice($bookingId, InvoiceItem::PAYMENT, $amount / 100);
-        $this->booking->setPaidStatus($bookingId, 'paid');
+        $this->paymentService->handleSuccessfulPayment($payload, $signature, $endpointSecret);
+
         return $this->json([]);
     }
 }
---
+++ src/Service/PaymentService.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Providers\Booking\Beds24\Entity\InvoiceItem;
+use App\Providers\Booking\BookingInterface;
+use Psr\Log\LoggerInterface;
+use Stripe\Webhook;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+
+class PaymentService
+{
+    public function __construct(
+        private readonly BookingInterface $booking,
+        private readonly LoggerInterface $logger,
+        private readonly ClientService $clientService,
+    ) {
+    }
+
+    public function handleSuccessfulPayment(string $payload, string $signature, string $endpointSecret): void
+    {
+        $event = Webhook::constructEvent($payload, $signature, $endpointSecret);
+        $paymentIntent = $event->data->object;
+        $bookingId = (int) $paymentIntent->metadata?->bookingId;
+        $clientName = $paymentIntent->metadata?->client;
+        $amount = $paymentIntent->amount;
+        if (!$bookingId) {
+            $this->logger->error('PaymentIntent does not contain bookingId', ['paymentIntent' => print_r($paymentIntent, true)]);
+            throw new BadRequestException('PaymentIntent does not contain bookingId');
+        }
+        if (!$clientName) {
+            $this->logger->error('PaymentIntent does not contain client name', ['paymentIntent' => print_r($paymentIntent, true)]);
+            throw new BadRequestException('PaymentIntent does not client name');
+        }
+        $this->booking->setToken($this->clientService->getTokenByName($clientName)->getToken());
+        $this->booking->addInvoice($bookingId, InvoiceItem::PAYMENT, $amount / 100);
+        $this->booking->setPaidStatus($bookingId, 'paid');
+    }
+}
