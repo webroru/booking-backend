0b50f862b2f19d4f639eadb9e213a013a0aef284
Igor Nemykin <i.nemykin@devcaz.com>
2023-02-04 12:18:36 +0100

    add notification service

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Dto\Booking;
 use App\Exception\TokenNotFoundException;
 use App\Serializer\Normalizer;
 use App\Service\ClientService;
+use App\Service\NotificationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +20,7 @@ class InfoController extends AbstractController
     public function __construct(
         private readonly Normalizer $normalizer,
         private readonly ClientService $clientService,
+        private readonly NotificationService $notificationService,
     ) {
     }
 
@@ -29,5 +32,16 @@ class InfoController extends AbstractController
             throw new TokenNotFoundException("Information for {$client->getName()} not found");
 
         return $this->json(['data' => $this->normalizer->normalize($info)]);
+    }
+
+    #[Route('/send-to-email', methods: ['POST'])]
+    public function sendToEmail(Request $request): JsonResponse
+    {
+        $bookingData = $request->get('booking');
+        $bookingDto = new Booking(...$bookingData);
+        $email = $request->get('email');
+        $this->notificationService->sendBookingDetails($bookingDto, $email);
+
+        return $this->json(['data' => 'ok']);
     }
 }
