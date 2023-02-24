fbd651df3ce3b364f320e77bbb12e449bee481b6
Igor Nemykin <i.nemykin@devcaz.com>
2023-02-24 15:53:00 +0100

    fix status filtration

--- src/Providers/Booking/Beds24/Booking.php
+++ src/Providers/Booking/Beds24/Booking.php
@@ -471,7 +471,7 @@ class Booking implements BookingInterface
             'arrivalTo' => $arrivalTo,
             'includeInvoiceItems' => true,
             'includeInfoItems' => true,
-            'status' => ['confirmed'],
+            'status' => ['confirmed', 'new'],
         ];
     }
 }
