f5c7a0fca3d0f770c182b9d6185304deb95407e8
Igor Nemykin <webroru@gmail.com>
2024-10-13 12:19:18 +0200

    fix CommonDtoConverterTest

--- tests/Providers/Booking/Beds24/CommonDtoConverterTest.php
+++ tests/Providers/Booking/Beds24/CommonDtoConverterTest.php
@@ -10,20 +10,40 @@ use App\Providers\Booking\Beds24\Entity\Booking;
 use App\Providers\Booking\Beds24\Entity\InfoItem;
 use App\Providers\Booking\Beds24\Entity\InvoiceItem;
 use App\Providers\Booking\Beds24\Entity\Property;
+use App\Repository\PhotoRepository;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class CommonDtoConverterTest extends TestCase
 {
     private Booking $booking;
     private Property $property;
+    private PhotoRepository|MockObject $photoRepositoryMock;
 
-    public function setUp(): void
+    public function testConvert(): void
     {
+        $converter = new CommonDtoConverter($this->photoRepositoryMock);
+        $bookingDTO = $converter->convert($this->booking, $this->property, []);
+        $this->assertInstanceOf(BookingDto::class, $bookingDTO);
+    }
+
+    protected function setUp(): void
+    {
+        $this->photoRepositoryMock = $this->getMockBuilder(PhotoRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->photoRepositoryMock->expects($this->once())
+            ->method('findBy')
+            ->willReturn([]);
+
         $invoiceItems = [new InvoiceItem(10, InvoiceItem::CHARGE, description: 'Test charging')];
         $infoItems = [new InfoItem('foo', '42')];
         $this->booking = new Booking(
             id: 1,
             referer: 'referer',
+            roomId: 1,
+            unitId: 1,
             arrival: '2000-01-01',
             departure: '2000-01-02',
             firstName: 'Test',
@@ -32,14 +52,9 @@ class CommonDtoConverterTest extends TestCase
             invoiceItems: $invoiceItems,
             infoItems: $infoItems,
         );
-        $this->property = new Property(1, 'Test', 'Test', [], 'Test', 'Test', 'Test', 'Test', 'Test', 'Test', 1.1, 1.1, 'Test', 'Test', 'Test', 'Test', 'Test', 'Test', 'Test', [], [], [], [], [], [], [], [], []);
-        parent::setUp();
-    }
 
-    public function testConvert(): void
-    {
-        $converter = new CommonDtoConverter();
-        $bookingDTO = $converter->convert($this->booking, $this->property);
-        $this->assertInstanceOf(BookingDto::class, $bookingDTO);
+        $roomType = ['id' => 1, 'name' => 'Test', 'units' => [], 'maxPeople' => 1, 'priceRules' => [['extraPerson' => 10]]];
+        $this->property = new Property(1, 'Test', 'Test', [], 'Test', 'Test', 'Test', 'Test', 'Test', 'Test', 1.1, 1.1, 'Test', 'Test', 'Test', 'Test', 'Test', 'Test', 'Test', [], [], [], [], [], [], [], [], [], [$roomType]);
+        parent::setUp();
     }
 }
