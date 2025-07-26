ac8aa00d9c85ea3bf1ee51e6ad4c75aeccf70646
Igor Nemykin <webroru@gmail.com>
2025-07-26 13:53:20 +0200

    [GREEN-81] fix migrations

--- migrations/Version20250726114715.php
+++ migrations/Version20250726114715.php
@@ -19,7 +19,6 @@ final class Version20250726114715 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE guest ADD client_id INT NOT NULL');
         $this->addSql('ALTER TABLE guest ADD CONSTRAINT FK_ACB79A3519EB6921 FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE');
         $this->addSql('CREATE INDEX IDX_ACB79A3519EB6921 ON guest (client_id)');
@@ -27,7 +26,6 @@ final class Version20250726114715 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE guest DROP FOREIGN KEY FK_ACB79A3519EB6921');
         $this->addSql('DROP INDEX IDX_ACB79A3519EB6921 ON guest');
         $this->addSql('ALTER TABLE guest DROP client_id');
