42a1f4a8eb1904cf3db0be0f7e714af7b3f34174
Igor Nemykin <webroru@gmail.com>
2025-07-26 13:53:20 +0200

    [GREEN-81] fix migrations

--- migrations/Version20250726090910.php
+++ migrations/Version20250726090910.php
@@ -19,15 +19,11 @@ final class Version20250726090910 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE beds24_token RENAME INDEX uniq_5f37a13b19eb6921 TO UNIQ_70146CDD19EB6921');
         $this->addSql('ALTER TABLE guest ADD referer VARCHAR(255) NOT NULL, ADD room VARCHAR(255) NOT NULL, ADD is_reported TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE beds24_token RENAME INDEX uniq_70146cdd19eb6921 TO UNIQ_5F37A13B19EB6921');
         $this->addSql('ALTER TABLE guest DROP referer, DROP room, DROP is_reported');
     }
 }
