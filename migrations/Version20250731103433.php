475699a49ab18675e83974c10bfdc47bb1b3dc47
Igor Nemykin <webroru@gmail.com>
2025-08-09 12:59:47 +0200

    fix migration

--- migrations/Version20250731103433.php
+++ migrations/Version20250731103433.php
@@ -20,7 +20,6 @@ final class Version20250731103433 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE beds24_token RENAME INDEX uniq_5f37a13b19eb6921 TO UNIQ_70146CDD19EB6921');
         $this->addSql('ALTER TABLE client ADD admin_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C7440455642B8210 FOREIGN KEY (admin_id) REFERENCES `admin` (id)');
         $this->addSql('CREATE INDEX IDX_C7440455642B8210 ON client (admin_id)');
@@ -29,7 +28,6 @@ final class Version20250731103433 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE beds24_token RENAME INDEX uniq_70146cdd19eb6921 TO UNIQ_5F37A13B19EB6921');
         $this->addSql('ALTER TABLE client DROP FOREIGN KEY FK_C7440455642B8210');
         $this->addSql('DROP INDEX IDX_C7440455642B8210 ON client');
         $this->addSql('ALTER TABLE client DROP admin_id');
