bfc21f019626ea3954069d647790a19291d0e86f
Igor Nemykin <igor.nemykin@dhl.com>
2025-08-22 16:56:29 +0200

    mode SoapClient to DI

---
+++ src/Factory/SoapClientFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Factory;
+
+use SoapClient;
+
+class SoapClientFactory
+{
+    public function create(string $wsdlPath): SoapClient
+    {
+        return new SoapClient($wsdlPath, [
+            'trace' => true,
+            'exceptions' => true,
+            'cache_wsdl' => WSDL_CACHE_NONE,
+//            'local_cert' => $certPath,
+//            'local_pk' => $keyPath,
+//            'passphrase' => $passphrase,
+        ]);
+    }
+}
